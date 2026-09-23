28023f7969416cff860ba3c5a236a6a6953c9eea
PintoJR <guimoraespinto@gmail.com>
2026-09-23 16:56:41 -0300

    Solution02

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-def create_report(data_file_name: str, report_file_name:str):
-
-    data_file_name = open("errors.csv", "r")
-
-
-
+def create_report(data_file_name: str, report_file_name: str):
+    with open(data_file_name, "r") as file:
+        total_supply = 0
+        total_buy = 0
+        result = 0   
+        for line in file:
+            if line.split(",")[0] == "supply":
+                total_supply += line.split(",")[1]
+            if line.split(",")[0] == "buy":
+                total_buy += line.split(",")[1]
+        result = total_supply - total_buy
+    with open(report_file_name, "w") as report:
+        report.write(f"supply,{total_supply}\n")
+        report.write(f"buy,{total_buy}\n")
+        report.write(f"result,{total_supply - total_buy}\n")
