4e4bd61dc178b6833124babba43ff79faa80c53e
PintoJR <guimoraespinto@gmail.com>
2026-09-23 17:20:38 -0300

    Solution03

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
-def create_report(data_file_name: str, report_file_name: str):
+def create_report(data_file_name: str, report_file_name: str) -> None:
     with open(data_file_name, "r") as file:
         total_supply = 0
         total_buy = 0
-        result = 0   
+        result = 0
         for line in file:
+            line = line.strip()
             if line.split(",")[0] == "supply":
-                total_supply += line.split(",")[1]
+                total_supply += int(line.split(",")[1])
             if line.split(",")[0] == "buy":
-                total_buy += line.split(",")[1]
+                total_buy += int(line.split(",")[1])
         result = total_supply - total_buy
     with open(report_file_name, "w") as report:
         report.write(f"supply,{total_supply}\n")
